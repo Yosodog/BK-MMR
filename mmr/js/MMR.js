d73f8cb43a8c4a4aeced1fe6649153f2f1109580
Yosodog <yosodog@gmail.com>
2016-11-26 23:07:29 -0500

    Different indenting

--- mmr/js/MMR.js
+++ mmr/js/MMR.js
@@ -1,118 +1,118 @@
 var MMR = function(cities) {
-	this.cities = cities;
+    this.cities = cities;
 
-	this.calcMMR = function() {
-		// Call all the methods to calculate MMR
-		this.calcFood();
-		this.calcGas();
-		this.calcMunitions();
-		this.calcSteel();
-		this.calcAluminum();
-		this.calcMoney();
-		this.calcTanks();
-		this.calcSoldiers();
-		this.calcShips();
-		this.calcAircraft();
-		this.calcMissiles();
-		this.calcSpies();
-	}
+    this.calcMMR = function() {
+        // Call all the methods to calculate MMR
+        this.calcFood();
+        this.calcGas();
+        this.calcMunitions();
+        this.calcSteel();
+        this.calcAluminum();
+        this.calcMoney();
+        this.calcTanks();
+        this.calcSoldiers();
+        this.calcShips();
+        this.calcAircraft();
+        this.calcMissiles();
+        this.calcSpies();
+    }
 
-	this.calcFood = function() {
-		if (this.cities < 3)
-			this.food = 0;
-		else if (cities < 8)
-			this.food = cities * 3000;
-		else
-			this.food = cities * 4000;
-	}
+    this.calcFood = function() {
+        if (this.cities < 3)
+            this.food = 0;
+        else if (cities < 8)
+            this.food = cities * 3000;
+        else
+            this.food = cities * 4000;
+    }
 
-	this.calcGas = function() {
-		if (this.cities < 3)
-			this.gas = 0;
-		else if (cities < 8)
-			this.gas = cities * 1500;
-		else
-			this.gas = cities * 2500;
-	}
+    this.calcGas = function() {
+        if (this.cities < 3)
+            this.gas = 0;
+        else if (cities < 8)
+            this.gas = cities * 1500;
+        else
+            this.gas = cities * 2500;
+    }
 
-	this.calcMunitions = function() {
-		if (this.cities < 3)
-			this.munitions = 0;
-		else if (this.cities < 8)
-			this.munitions = cities * 2000;
-		else
-			this.munitions = cities * 2500;
-	}
+    this.calcMunitions = function() {
+        if (this.cities < 3)
+            this.munitions = 0;
+        else if (this.cities < 8)
+            this.munitions = cities * 2000;
+        else
+            this.munitions = cities * 2500;
+    }
 
-	this.calcSteel = function() {
-		if (this.cities < 3)
-			this.steel = 0;
-		else if (this.cities < 8)
-			this.steel = cities * 2000;
-		else
-			this.steel = cities * 2500;
-	}
+    this.calcSteel = function() {
+        if (this.cities < 3)
+            this.steel = 0;
+        else if (this.cities < 8)
+            this.steel = cities * 2000;
+        else
+            this.steel = cities * 2500;
+    }
 
-	this.calcAluminum = function() {
-		if (this.cities < 3)
-			this.aluminum = 0;
-		else if (this.cities < 8)
-			this.aluminum = cities * 660;
-		else
-			this.aluminum = cities * 742;
-	}
+    this.calcAluminum = function() {
+        if (this.cities < 3)
+            this.aluminum = 0;
+        else if (this.cities < 8)
+            this.aluminum = cities * 660;
+        else
+            this.aluminum = cities * 742;
+    }
 
-	this.calcMoney = function() {
-		if (this.cities < 3)
-			this.money = 0;
-		else if (this.cities < 5)
-			this.money = cities * 1200000;
-		else if (this.cities < 9)
-			this.money = cities *1500000;
-		else
-			this.money = cities * 1700000;
-	}
+    this.calcMoney = function() {
+        if (this.cities < 3)
+            this.money = 0;
+        else if (this.cities < 5)
+            this.money = cities * 1200000;
+        else if (this.cities < 9)
+            this.money = cities *1500000;
+        else
+            this.money = cities * 1700000;
+    }
 
-	this.calcTanks = function() {
-		// This should always be run BEFORE calcSoldiers()
-		if (this.cities < 3)
-			this.tanks = cities * 75;
-		else if (this.cities < 8)
-			this.tanks = cities * 100;
-		else
-			this.tanks = cities * 750;
-	}
+    this.calcTanks = function() {
+        // This should always be run BEFORE calcSoldiers()
+        if (this.cities < 3)
+            this.tanks = cities * 75;
+        else if (this.cities < 8)
+            this.tanks = cities * 100;
+        else
+            this.tanks = cities * 750;
+    }
 
-	this.calcSoldiers = function() {
-		if (this.cities < 6)
-			this.soldiers = cities * 6000;
-		else
-			this.soldiers = this.tanks * 0.84;
-	}
+    this.calcSoldiers = function() {
+        if (this.cities < 6)
+            this.soldiers = cities * 6000;
+        else
+            this.soldiers = this.tanks * 0.84;
+    }
 
-	this.calcAircraft = function() {
-		if (this.cities < 3)
-			this.aircraft = cities * 5;
-		else if (this.cities < 5)
-			this.aircraft = cities * 10;
-		else
-			this.aircraft = cities * 54;
-	}
+    this.calcAircraft = function() {
+        if (this.cities < 3)
+            this.aircraft = cities * 5;
+        else if (this.cities < 5)
+            this.aircraft = cities * 10;
+        else
+            this.aircraft = cities * 54;
+    }
 
-	this.calcShips = function() {
-		this.ships = 0;
-	}
+    this.calcShips = function() {
+        this.ships = 0;
+    }
 
-	this.calcMissiles = function() {
-		this.missiles = 0;
-	}
+    this.calcMissiles = function() {
+        this.missiles = 0;
+    }
 
-	this.calcSpies = function() {
-		if (this.cities < 3)
-			this.spies = 0;
-		else if (this.cities < 8)
-			this.spies = 25;
-		else
-			this.spies = 50;
-	}
+    this.calcSpies = function() {
+        if (this.cities < 3)
+            this.spies = 0;
+        else if (this.cities < 8)
+            this.spies = 25;
+        else
+            this.spies = 50;
+    }
 };
--- mmr/js/MMRScore.js
+++ mmr/js/MMRScore.js
@@ -1,85 +1,85 @@
 var MMRScore = function(cities, food, gas, munitions, steel, aluminum) {
-	this.food = food;
-	this.gas = gas;
-	this.munitions = munitions;
-	this.steel = steel;
-	this.aluminum = aluminum;
+    this.food = food;
+    this.gas = gas;
+    this.munitions = munitions;
+    this.steel = steel;
+    this.aluminum = aluminum;
 
-	// Calculate the nation's MMR
-	this.mmr = new MMR(cities);
-	this.mmr.calcMMR();
+    // Calculate the nation's MMR
+    this.mmr = new MMR(cities);
+    this.mmr.calcMMR();
 
-	this.calcMMRScore = function() {
-		this.calcFood();
-		this.calcGas();
-		this.calcMunitions();
-		this.calcSteel();
-		this.calcAluminum();
-		this.addEverything();
-	}
+    this.calcMMRScore = function() {
+        this.calcFood();
+        this.calcGas();
+        this.calcMunitions();
+        this.calcSteel();
+        this.calcAluminum();
+        this.addEverything();
+    }
 
-	this.calcFood = function() {
-		// Calculate how much of MMR has been met
-		var percOfMMR = this.food / this.mmr.food;
+    this.calcFood = function() {
+        // Calculate how much of MMR has been met
+        var percOfMMR = this.food / this.mmr.food;
 
-		// If the value is over 1, just set it to 1
-		if (percOfMMR > 1)
-			percOfMMR = 1;
+        // If the value is over 1, just set it to 1
+        if (percOfMMR > 1)
+            percOfMMR = 1;
 
-		// Now weigh it
-		this.foodWeight = percOfMMR * 0.05;
-	}
+        // Now weigh it
+        this.foodWeight = percOfMMR * 0.05;
+    }
 
-	this.calcGas = function() {
-		// Calculate how much of MMR has been met
-		var percOfMMR = this.gas / this.mmr.gas;
+    this.calcGas = function() {
+        // Calculate how much of MMR has been met
+        var percOfMMR = this.gas / this.mmr.gas;
 
-		// If the value is over 1, just set it to 1
-		if (percOfMMR > 1)
-			percOfMMR = 1;
+        // If the value is over 1, just set it to 1
+        if (percOfMMR > 1)
+            percOfMMR = 1;
 
-		// Now weigh it
-		this.gasWeight = percOfMMR * 0.3;
-	}
+        // Now weigh it
+        this.gasWeight = percOfMMR * 0.3;
+    }
 
-	this.calcMunitions = function() {
-		// Calculate how much of MMR has been met
-		var percOfMMR = this.munitions / this.mmr.munitions;
+    this.calcMunitions = function() {
+        // Calculate how much of MMR has been met
+        var percOfMMR = this.munitions / this.mmr.munitions;
 
-		// If the value is over 1, just set it to 1
-		if (percOfMMR > 1)
-			percOfMMR = 1;
+        // If the value is over 1, just set it to 1
+        if (percOfMMR > 1)
+            percOfMMR = 1;
 
-		// Now weigh it
-		this.munitionsWeight = percOfMMR * 0.25;
-	}
+        // Now weigh it
+        this.munitionsWeight = percOfMMR * 0.25;
+    }
 
-	this.calcSteel = function() {
-		// Calculate how much of MMR has been met
-		var percOfMMR = this.steel / this.mmr.steel;
+    this.calcSteel = function() {
+        // Calculate how much of MMR has been met
+        var percOfMMR = this.steel / this.mmr.steel;
 
-		// If the value is over 1, just set it to 1
-		if (percOfMMR > 1)
-			percOfMMR = 1;
+        // If the value is over 1, just set it to 1
+        if (percOfMMR > 1)
+            percOfMMR = 1;
 
-		// Now weigh it
-		this.steelWeight = percOfMMR * 0.3;
-	}
+        // Now weigh it
+        this.steelWeight = percOfMMR * 0.3;
+    }
 
-	this.calcAluminum = function() {
-		// Calculate how much of MMR has been met
-		var percOfMMR = this.aluminum / this.mmr.aluminum;
+    this.calcAluminum = function() {
+        // Calculate how much of MMR has been met
+        var percOfMMR = this.aluminum / this.mmr.aluminum;
 
-		// If the value is over 1, just set it to 1
-		if (percOfMMR > 1)
-			percOfMMR = 1;
+        // If the value is over 1, just set it to 1
+        if (percOfMMR > 1)
+            percOfMMR = 1;
 
-		// Now weigh it
-		this.aluminumWeight = percOfMMR * 0.1;
-	}
+        // Now weigh it
+        this.aluminumWeight = percOfMMR * 0.1;
+    }
 
-	this.addEverything = function() {
-		var mmrScore = (this.foodWeight + this.gasWeight + this.munitionsWeight + this.steelWeight + this.aluminumWeight) * 100;
-		this.mmrScore = mmrScore.toFixed(2);
-	}
+    this.addEverything = function() {
+        var mmrScore = (this.foodWeight + this.gasWeight + this.munitionsWeight + this.steelWeight + this.aluminumWeight) * 100;
+        this.mmrScore = mmrScore.toFixed(2);
+    }
 }
